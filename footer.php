dc6bf0064ee07482e7dfac530fe894f2c98f2985
AishwaryBajpai <87269715+AishwaryBajpai@users.noreply.github.com>
2022-06-01 10:19:06 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
             <div class="container">
                 <center>
-                    <p>Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 90000 00000</p>	
+                    <p>Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 89999 99999</p>	
                 </center>
             </div>
  </footer>
